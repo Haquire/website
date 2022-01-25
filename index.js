74fa0e050cb0026a304742d5dde47fcca49bd622
Haquire <93946567+Haquire@users.noreply.github.com>
2022-01-25 13:33:24 -0500

    Create Index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function toCelsius(f){
+    var result = (f-32) * (5/9);
+    return result;
+};
+
+function toFarenheit(c){
+    var result = (c * 9/5) + 32;
+    return result;
+};
+
+
+
+document.getElementById("atcelc").onclick = function(){
+    var ree = document.getElementById("celsius").value;
+    result = toCelsius(ree);
+    var fixedNum = parseFloat(result).toFixed( 3 );
+    console.log(fixedNum)
+}
+
+document.getElementById("farenbutton").onclick = function(){
+    var ree = document.getElementById("Farenheit").value;
+    result = toFarenheit(ree);
+    var fixedNum = parseFloat(result).toFixed( 3 );
+    console.log(fixedNum)
+}
